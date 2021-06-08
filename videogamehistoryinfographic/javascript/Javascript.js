614892b07b6c556570c36453cd26b2bd03109d73
DHindriks <dl.hindriks@gmail.com>
2021-06-08 11:08:58 +0200

    Added page on Airrach design, removed unused content

--- videogamehistoryinfographic/javascript/Javascript.js
+++
@@ -1,24 +0,0 @@
-window.addEventListener('scroll', checkInView);
-window.addEventListener('resize', checkInView);
-
-function checkInView(){
-   // current pageview position
-   let windowHeight = window.innerHeight;
-   let windowTop = window.pageYOffset;
-   let windowBottom = windowTop + windowHeight;
-
-   // Select all elements to be animated
-   let elements = document.querySelectorAll('.animate');
-   for(element of elements) {   
-      // element offsets relative to viewport
-      let elementRect = element.getBoundingClientRect();
-      // check is item is in view
-      if(elementRect.top < windowHeight && elementRect.bottom > 0 ){
-         element.classList.add('fadeIn');
-         console.log("check");
-      }
-
-   }
-
-
-}
